1cf83cdef825d6acc418892393bbec6afbcf3644
fleeB1 <fowler.lee8@gmail.com>
2023-04-06 20:32:56 +0200

    commit unsafe

--- myunsafe/src/lib.rs
+++ myunsafe/src/lib.rs
@@ -1,10 +1,13 @@
 #[allow(dead_code)]
 fn unsafe_raw_pointer() -> i32 {
     let mut value = 42;
+    let v = 20i32;
     let raw_ptr = &mut value as *mut i32; // Getting a raw mutable pointer
+    let raw_num = &v as *const i32;
 
     unsafe {
-        *raw_ptr += 1; // Modifying data through a raw pointer
+        *raw_ptr += 1 + *raw_num; // Modifying data through a raw pointer
+        
     }
 
     println!("Value after increment: {}", value);
@@ -17,6 +20,6 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        assert_eq!(unsafe_raw_pointer(), 42);
+        assert_eq!(unsafe_raw_pointer(), 63);
     }
 }
