ea46e3818ea18d14665c2dbde393b2d7b801619d
fleeB1 <fowler.lee8@gmail.com>
2023-05-14 13:06:41 +0200

    list that is not thread safe

---
+++ data/structures/src/dlist.rs
@@ -0,0 +1,89 @@
+use std::cell::{Ref, RefCell};
+use std::collections::LinkedList;
+use std::io::SeekFrom;
+use std::rc::Rc;
+
+#[derive(Clone)]
+struct Node {
+    value: String,
+    next: Link,
+    prev: Link,
+}
+
+type Link = Option<Rc<RefCell<Node>>>;
+
+impl Node {
+    fn new(value: String) -> Rc<RefCell<Node>> {
+        Rc::new(RefCell::new(Node {
+            value: (value),
+            next: (None),
+            prev: (None),
+        }))
+    }
+}
+
+#[derive(Clone)]
+pub struct Logger {
+    head: Link,
+    tail: Link,
+    pub length: u64,
+}
+
+impl Logger {
+    pub fn new() -> Self {
+        Self {
+            head: (None),
+            tail: (None),
+            length: (0),
+        }
+    }
+
+    pub fn append(&mut self, value: String) {
+        let new = Node::new(value);
+        match self.tail.take() {
+            Some(old) => {
+                old.borrow_mut().next = Some(new.clone());
+                new.borrow_mut().prev = Some(old);
+            }
+            None => self.head = Some(new.clone()),
+        }
+        self.length += 1;
+        self.tail = Some(new)
+    }
+
+    pub fn pop(&mut self) -> Option<String> {
+        self.head.take().map(|head| {
+            if let Some(next) = head.borrow_mut().next.take() {
+                next.borrow_mut().prev = None;
+                self.head = Some(next);
+            } else {
+                self.tail.take();
+            }
+            self.length -= 1;
+            Rc::try_unwrap(head)
+                .ok()
+                .expect("Error occurred when popping off list")
+                .into_inner()
+                .value
+        })
+    }
+}
+
+// pub struct ListIterator {
+//     current: Link,
+// }
+
+// impl ListIterator {
+//     fn new(start_at: Link) -> Self {
+//         Self { current: start_at }
+//     }
+// }
+
+// impl IntoIterator for Logger {
+//     type Item = String;
+//     type IntoIter = ListIterator;
+
+//     fn into_iter(self) -> Self::IntoIter {
+//         todo!()
+//     }
+// }
