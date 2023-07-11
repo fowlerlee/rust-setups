34a1532287b59d175893d275a7db132cfb71188c
lee fowler <fowler.lee8@gmail.com>
2023-07-11 20:32:06 +0200

    add tests and drop the lock

--- blocker/src/channy.rs
+++ blocker/src/channy.rs
@@ -1,7 +1,8 @@
 use std::{
     cell::UnsafeCell,
     mem::MaybeUninit,
-    sync::atomic::{AtomicI8, AtomicU8},
+    sync::atomic::AtomicU8,
+    sync::atomic::Ordering::{Acquire, Relaxed, Release},
 };
 
 const EMPTY: u8 = 0;
@@ -23,4 +24,51 @@ impl<T> Channy<T> {
             state: AtomicU8::new(EMPTY),
         }
     }
+
+    pub fn send(&self, message: T) {
+        if self
+            .state
+            .compare_exchange(EMPTY, WRITING, Relaxed, Relaxed)
+            .is_err()
+        {
+            panic!("can't send more than one message!")
+        }
+        unsafe {
+            (*self.message.get()).write(message);
+        }
+        self.state.store(READY, Release);
+    }
+
+    pub fn is_ready(&self) -> T {
+        if self
+            .state
+            .compare_exchange(READY, READING, Acquire, Relaxed)
+            .is_err()
+        {
+            panic!("no message available")
+        }
+        unsafe { (*self.message.get()).assume_init_read() }
+    }
+}
+
+impl<T> Drop for Channy<T> {
+    fn drop(&mut self) {
+        if *self.state.get_mut() == READY {
+            unsafe { self.message.get_mut().assume_init_drop() }
+        }
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn test_unsafe_channy() {
+       let ch: Channy<String>  = Channy::new();
+       ch.send("letter".to_string());
+    }
+
+    #[test]
+    fn test_channy() {}
 }
---
+++ blocker/src/swappy.rs
@@ -0,0 +1,78 @@
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::*;
+use std::sync::mpsc::{Receiver, Sender};
+use std::{sync::mpsc, thread};
+
+unsafe impl<T> Send for SwapMutex<T> {}
+unsafe impl<T> Sync for SwapMutex<T> {}
+
+pub struct SwapMutex<T> {
+    locked: AtomicBool,
+    data: *mut T,
+}
+
+impl<T> SwapMutex<T> {
+    fn new(t: T) -> Self {
+        Self {
+            locked: AtomicBool::new(false),
+            data: Box::into_raw(Box::new(t)),
+        }
+    }
+}
+
+fn main() {
+    let s = SwapMutex::new(1u32);
+
+    let t = thread::spawn(move || {
+        match s
+            .locked
+            .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
+        {
+            Ok(x) => {
+                println!("result Ok: {:?}", x);
+            }
+            Err(y) => println!("result Err: {:?}", y),
+        }
+    });
+}
+
+#[cfg(test)]
+mod tests {
+    use std::cell::UnsafeCell;
+
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn test_channel() {
+        let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+        let handles = (0..10).map(|i| {
+            let tx2 = tx.clone();
+            thread::spawn(move || {
+                tx2.send(i).expect("could not send");
+            })
+        });
+        for h in handles {
+            h.join().unwrap();
+        }
+        let mut v: Vec<i32> = Vec::with_capacity(10);
+        v.push(rx.recv().unwrap());
+    }
+
+    #[test]
+    fn test_spinlock() {
+        let s = SwapMutex::new(1u32);
+
+        let t = thread::spawn(move || {
+            match s
+                .locked
+                .compare_exchange(false, true, Ordering::Relaxed, Ordering::Relaxed)
+            {
+                Ok(x) => {
+                    println!("result Ok: {:?}", x);
+                }
+                Err(y) => println!("result Err: {:?}", y),
+            }
+        });
+    }
+}
